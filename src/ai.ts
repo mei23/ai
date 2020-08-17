96d59a3ec2869dfb9491a59333db7d5fb498e5be
mei23 <m@m544.net>
2020-08-17 22:10:45 +0900

    ping

--- src/ai.ts
+++ src/ai.ts
@@ -119,7 +119,7 @@ export default class 藍 {
 
 		setInterval(() => {
 			this.log('Test API request to stream');
-			this.connection.api('meta')
+			this.connection.rawSend('ping');
 		}, 10 * 60 * 1000);
 
 		//#region Main stream
