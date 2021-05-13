13d1dbb5eb0c9107ef83df3c832c01c7d03821d5
mei23 <m@m544.net>
2021-05-14 04:37:42 +0900

    revert

--- src/modules/keyword/index.ts
+++ src/modules/keyword/index.ts
@@ -51,7 +51,7 @@ export default class extends Module {
 
 		for (const note of interestedNotes) {
 			const tokens = await mecab(note.text, config.mecab, config.mecabDic);
-			const keywordsInThisNote = tokens.filter(token => token[1] == '名詞' && token[8] != null);
+			const keywordsInThisNote = tokens.filter(token => token[2] == '固有名詞' && token[8] != null);
 			keywords = keywords.concat(keywordsInThisNote);
 		}
 
